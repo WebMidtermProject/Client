2b474862fed7f5ca59d2e40de1216c1623a3aeda
vietnguyenjr <vietnguyen@busmap.vn>
2022-12-01 19:36:10 +0700

    fix route

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -17,28 +18,27 @@ const Content = (props) => {
   console.log(user);
   return (
     <article className="content">
-{/*       <Routes>
-        <Route path="/" element={<h2>Profile</h2>} />
-        <Route path="/profile" elsement={<h2>Profile</h2>} />
+      <Routes>
+        <Route path="/" element={user ? <><h2>Profile</h2><Profile></Profile></> : <SignInPage></SignInPage>} />
+        <Route path="/profile" element={<h2>Profile</h2>} />
         <Route path="/my-groups" element={<h2>My groups</h2>} />
-        <Route path="/joined-groups" element={<h2>Joined groups</h2>} />
-        <Route path="/create-group" element={<h2>Create Group</h2>} />
+        <Route path="/joined-groups" element={user ? <><h2>Joined groups</h2><ShowGroups></ShowGroups></>: <SignInPage></SignInPage>} />
+        <Route path="/create-group" element={ user ? <><h2>Create Group</h2><CreateGroupForm></CreateGroupForm></>: <SignInPage></SignInPage>} />
         <Route path="/group/:id" element={<h2>Group Detail</h2>} />
         <Route path="/about" element={<h2>About</h2>} />
-      </Routes> */}
+      </Routes>
 
-        <Routes>
-          <Route path="/sign-in" element={user ? <Navigate to="/profile " /> : <SignInPage></SignInPage>} />
-{/*           <Route
+        {/* <Routes>
+          <Route path="/sign-in" element={user ? <Navigate to="/profile" /> : <SignInPage></SignInPage>} />
+          <Route
             path="/"
             element={user ? <Profile></Profile> : <Navigate to="/sign-in" />}
           />
           <Route
             path="/profile"
             element={user ? <Profile></Profile> : <Navigate to="/sign-in" />}
-          /> */}
-          <Route path="/create-group" element={user ? <CreateGroupForm></CreateGroupForm> : <Navigate to="/ccccc" />} />
-      </Routes>
+          />
+      </Routes> */}
     </article>
   );
 };
