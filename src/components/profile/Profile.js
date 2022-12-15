935795b0fd7c1cb2cdb7b34b6a360b4c70b798cf
Phan Vu <npvu19@clc.fitus.edu.vn>
2022-12-15 17:41:35 +0700

    modal implementation

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 
 import ShowGroups from "../group/ShowGroups";
 import "./css/Profile.css";
+import "./css/Modal.css";
 
 const Profile = (props) => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const Profile = (props) => {
   const [inputName, setInputName] = useState("");
   const [edit, setEdit] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
+  const [modal, setModal] = useState(false);
   const handleGroupClick = (suffix) => {
     navigate(suffix);
   };
@@ -35,6 +37,11 @@ const Profile = (props) => {
 
   const handleOnChange = async (e) => {
     setInputName(e.target.value);
+  };
+
+  const toggleModal = () => {
+    setModal(!modal);
+    console.log(modal);
   };
 
   useEffect(() => {
@@ -64,86 +71,109 @@ const Profile = (props) => {
   }, [getProfileUrl]);
 
   return (
-    <Container className="profile">
-      <div className="profile--sub">
-        <section className="profile__info">
-          <div className="profile__info--general">
-            <img
-              className="profile__img"
-              src={
-                userInfo?.picture
-                  ? userInfo?.picture
-                  : "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg"
-              }
-              alt="default img profile"
-            />
-            {!edit && (
-              <>
-                <span className="profile__fullname">
-                  {userInfo?.first_name
-                    ? userInfo?.first_name + userInfo?.last_name
-                    : "User Fullname"}
-                </span>
-                <input
-                  type="button"
-                  name="change-name"
-                  value="change"
-                  onClick={() => setEdit(true)}
+    <>
+      {!modal && (
+        <Container className="profile">
+          <div className="profile--sub">
+            <section className="profile__info">
+              <div className="profile__info--general">
+                <img
+                  className="profile__img"
+                  src={
+                    userInfo?.picture
+                      ? userInfo?.picture
+                      : "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg"
+                  }
+                  alt="default img profile"
                 />
-              </>
-            )}
+                {!edit && (
+                  <>
+                    <span className="profile__fullname">
+                      {userInfo?.first_name
+                        ? userInfo?.first_name + userInfo?.last_name
+                        : "User Fullname"}
+                    </span>
+                    <input
+                      type="button"
+                      name="change-name"
+                      value="change"
+                      onClick={() => setEdit(true)}
+                    />
+                  </>
+                )}
 
-            {edit && (
-              <>
-                <input
-                  type="text"
-                  onChange={handleOnChange}
-                  value={inputName}
-                ></input>
-                <input
-                  type="button"
-                  name="change-name"
-                  value="OK"
-                  onClick={handleChangeName}
-                />
-              </>
-            )}
-          </div>
-          <div className="profile__info--detail">
-            <h3>Description</h3>
-            <div className="profile__info__description">
-              <p>
-                {props.user && props.user.description
-                  ? props.user.description
-                  : "Nothing to show"}
-              </p>
-            </div>
-          </div>
-        </section>
+                {edit && (
+                  <>
+                    <input
+                      type="text"
+                      onChange={handleOnChange}
+                      value={inputName}
+                    ></input>
+                    <input
+                      type="button"
+                      name="change-name"
+                      value="OK"
+                      onClick={handleChangeName}
+                    />
+                  </>
+                )}
+              </div>
+              <div className="profile__info--detail">
+                <h3>Description</h3>
+                <div className="profile__info__description">
+                  <p>
+                    {props.user && props.user.description
+                      ? props.user.description
+                      : "Nothing to show"}
+                  </p>
+                </div>
+              </div>
+            </section>
 
-        <section className="profile__group">
-          <h3 className="profile__group__label">Groups</h3>
-          <div className="profile__group--two-col">
-            <div
-              className="profile_group__left"
-              onClick={() => handleGroupClick("/my-groups")}
-            >
-              <ShowGroups profile={true} title={"My groups"}></ShowGroups>
-            </div>
-            <div
-              className="profile_group__right"
-              onClick={() => handleGroupClick("/joined-groups")}
-            >
-              <ShowGroups
-                type="joined groups"
-                title={"Joined groups"}
-                profile={true}
-              ></ShowGroups>
+            <section className="profile__group">
+              <h3 className="profile__group__label">Groups</h3>
+              <div className="profile__group--two-col">
+                <div
+                  className="profile_group__left"
+                  onClick={() => handleGroupClick("/my-groups")}
+                >
+                  <ShowGroups profile={true} title={"My groups"}></ShowGroups>
+                </div>
+                <div
+                  className="profile_group__right"
+                  onClick={() => handleGroupClick("/joined-groups")}
+                >
+                  <ShowGroups
+                    type="joined groups"
+                    title={"Joined groups"}
+                    profile={true}
+                  ></ShowGroups>
+                </div>
+              </div>
+            </section>
+          </div>
+          <button className="btn" onClick={toggleModal}>
+            Modal
+          </button>
+        </Container>
+      )}
+
+      {modal && (
+        <div className="create-group-modal">
+          <div className="create-group-modal-overlay">
+            <div className="create-group-modal-content">
+              <h1 className="create-group-modal-content__title">MODAL</h1>
+              <button
+                className="create-group-modal-content__close-btn"
+                onClick={toggleModal}
+              >
+                X
+              </button>
             </div>
           </div>
-        </section>
-      </div>
-    </Container>
+        </div>
+      )}
+    </>
   );
 };
 
